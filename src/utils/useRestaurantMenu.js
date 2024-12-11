97c582b1917aaec90b9965bdec3ead553adbbe98
Ayush <heyayush87@gmail.com>
2024-12-11 22:05:02 +0530

    fixed some api

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -2,19 +2,20 @@ import { useState, useEffect } from "react";
 
 const useRestaurantMenu = (id) => {
   const [resMenu, setResMenu] = useState([]);
+  console.log(id);
 
   const fetchResDetails = async () => {
     //RESTAURANTMENU DATA IS ALSO HARDCORE BUT WE CANT USE IT BCZ WE NEED DYNAMIC ID OF EACH RESTAURANT
     // LAST DATE I CHANGED MENU API IS 2 DECEMBER AND IN NEW API MENU IS AT CARDS[5]INSTEAD OF 4
     try {
       const data = await fetch(
-        " https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9532395&lng=77.70156639999999&restaurantId=237665&submitAction=ENTER"
+        `https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9532395&lng=77.70156639999999&restaurantId=${id}&submitAction=ENTER`
       );
       if (!data.ok) {
         throw new Error(`from menu,${data.status}`);
       }
       const json = await data.json();
-      // console.log("from restaurant MEnu APi", json);
+      console.log("from restaurant MEnu APi", json);
 
       const category =
         json?.data.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
